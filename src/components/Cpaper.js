aacea217eb2a7ee3e22a530efa3cf7ff572cc1a4
Asatter833 <asatter833@gmail.com>
2022-10-27 15:03:06 +0600

    Update Category Page

--- src/components/Cpaper.js
+++ src/components/Cpaper.js
@@ -5,6 +5,12 @@ const CPaper = styled(Paper)(({ theme }) => ({
   padding: "10px",
   boxShadow: "unset",
   border: "2px solid #00000011",
+  "& > .MuiTypography-root": {
+    margin: "10px 0",
+  },
+  "& > .MuiTypography-root:first-child": {
+    marginTop: "0",
+  },
 }));
 
 export default CPaper;
