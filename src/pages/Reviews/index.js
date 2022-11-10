101b175fd28b5de62920dd440d43dbdab68f33d4
Asatter833 <asatter833@gmail.com>
2022-11-10 23:31:18 +0600

    Review Page

--- src/pages/Reviews/index.js
+++ src/pages/Reviews/index.js
@@ -6,12 +6,25 @@ import {
   Select,
   Container,
   Typography,
+  Stack,
+  Avatar,
+  Rating,
+  ListItem,
+  ListItemAvatar,
 } from "@mui/material";
+import { blue } from "@mui/material/colors";
+import { Box } from "@mui/system";
+import moment from "moment";
 
 import React from "react";
+import { IoIosImages } from "react-icons/io";
 import snackContext from "../../context/snackProvider";
 import { useGetAllOrder } from "../../query/order";
+import { useGetAllReview } from "../../query/review";
+import { getAttachment } from "../../service/instance";
 import tableOptionsStyle from "../../style/tableOptions";
+import ReviewBoxes from "./ReviewBoxes";
+import ReviewBoxess from "./ReviewBoxes";
 
 const Index = () => {
   const snack = React.useContext(snackContext);
@@ -22,7 +35,14 @@ const Index = () => {
     filters: [],
   });
 
-  const { data, isLoading } = useGetAllOrder(params);
+  const { data: reviewList, isLoading } = useGetAllReview(params);
+
+  const [reviewInfo, setReviewInfo] = React.useState([]);
+
+  React.useEffect(() => {
+    setReviewInfo(reviewList?.data?.data || []);
+  }, [isLoading]);
+  console.log(reviewInfo);
   return (
     <>
       <Container
@@ -119,8 +139,17 @@ const Index = () => {
             </Grid>
           </Grid>
         </Paper>
-        <Paper>
-          <Typography>All Reviews</Typography>
+        <Typography
+          variant="h6"
+          sx={{
+            mb: 1,
+            fontWeight: "bold",
+          }}
+        >
+          All Review
+        </Typography>
+        <Paper sx={{ p: 2 }}>
+          <ReviewBoxes />
         </Paper>
       </Container>
     </>
